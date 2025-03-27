e057ac7a30506e3040acac670e21ddb38da2e2a8
swjeong9 <wjdtmddn1324@gmail.com>
2025-03-27 05:52:31 +0000

    designate stage via node ip

--- 2_pp_infer.py
+++ 2_pp_infer.py
@@ -1,7 +1,55 @@
+import os
+from typing import List, Dict
 from vllm import AsyncEngineArgs, AsyncLLMEngine, SamplingParams
 from vllm.usage.usage_lib import UsageContext
-
+import ray
 from torch.distributed import destroy_process_group
+
+node_rank_mapping = {
+    "172.31.16.230": 0,
+    "172.31.9.40": 2,
+    "172.31.24.180": 1,
+}
+
+def create_placement_group_and_bundle_indices():
+    if not ray.is_initialized():
+        ray.init(address="auto")
+
+    nodes = ray.nodes()
+    ips = [ip for ip, rank in node_rank_mapping.items()]
+
+    placement_group_specs: List[Dict[str, float]] = []
+    for ip in ips:
+        placement_group_specs.append({
+            'GPU': 1,
+            f"node:{ip}": 0.001 # 특정 노드를 사용하겠다는 의미
+        })
+    
+    placement_group = ray.util.placement_group(placement_group_specs, strategy="STRICT_SPREAD")
+    ray.get(placement_group.ready())
+
+    bundle_to_node = {}
+    for bundle_id, bundle in enumerate(placement_group.bundle_specs):
+        for resource_key in bundle:
+            if resource_key.startswith("node:"):
+                node_ip = resource_key[5:] # 'node:172.31.16.230' -> '172.31.16.230'
+                bundle_to_node[bundle_id] = node_ip
+    
+    bundle_indices = []
+    for rank in range(len(node_rank_mapping)):
+        for bundle_idx, node_ip in bundle_to_node.items():
+            if node_rank_mapping[node_ip] == rank:
+                bundle_indices.append(str(bundle_idx))
+                break
+            
+    os.environ["VLLM_RAY_BUNDLE_INDICES"] = ",".join(bundle_indices)
+    print(f"VLLM_RAY_BUNDLE_INDICES is setted to {os.environ['VLLM_RAY_BUNDLE_INDICES']}")
+    print(f"bundle specs : {placement_group.bundle_specs}")
+
+    # 우선 매뉴얼하게 박아보자
+    os.environ["VLLM_PP_LAYER_PARTITION"] = "12,14,6"
+
+    return placement_group
 
 
 async def main():
@@ -14,7 +62,7 @@ async def main():
         "task": task,
         "dtype": dtype,
         "tensor_parallel_size": 1,
-        "pipeline_parallel_size": 2,
+        "pipeline_parallel_size": 3,
     }
     engine_args = AsyncEngineArgs(**engine_args)
     usage_context = UsageContext.ENGINE_CONTEXT
@@ -23,6 +71,9 @@ async def main():
     # 예를 들어 V1 을 사용하려 했으나 Compute Capacity 가 80아래면 V0 로 Fall back 하게 됨
     vllm_config = engine_args.create_engine_config(usage_context=usage_context)
     vllm_config.model_config.max_model_len = 4096
+
+    placement_group = create_placement_group_and_bundle_indices()
+    vllm_config.parallel_config.placement_group = placement_group
 
     # 아래 코드에서 tokenizer 와 model 의 safetensor 를 받아오며
     # CUDA graph shape capturing 을 한다.
@@ -48,6 +99,8 @@ async def main():
 
     # get the results
     final_output = None
+
+    # 실행한 메인 스레드에서만 출력하게 설정되어 있음
     async for request_output in results_generator:
         for text_output in request_output.outputs:
             print(f"Prompt: {prompt}")
