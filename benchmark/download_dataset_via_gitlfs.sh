854abd01cc32f8af56c030049b3a08fb1c6ff232
swjeong9 <wjdtmddn1324@gmail.com>
2025-03-31 04:46:56 +0000

    add benchmarking code

---
+++ benchmark/download_dataset_via_gitlfs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Git 에서 직접 클론
+# Url 인자와 output dir 인자를 반드시 받아야 함
+# 인자가 두 개인지 확인하고 아니라면 Usage 출력
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <url> <output_dir>"
+    echo "Example: $0 https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered ./sharegpt"
+    exit 1
+fi
+
+# git lfs 가 반드시 설치되어 있어야함.
+if ! command -v git-lfs &> /dev/null; then
+    echo "git-lfs is not installed. Please install it first."
+    exit 1
+fi
+
+url=$1
+output_dir=$2
+
+git clone $url $output_dir
+
+cd $output_dir
+git lfs intall
+git lfs pull
